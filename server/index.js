fa6bd75231512b867b5aa1baf1ba921c7ac7475c
Laramie Downs <laramiedowns1994@gmail.com>
2022-01-05 11:43:04 -0600

    fixed npm start issue

--- server/index.js
+++ server/index.js
@@ -2,17 +2,20 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const app = express()
+
 require('dotenv').config
-const {SERVER_PORT} = process.env.PORT
+const SERVER_PORT = process.env.PORT || 4545
 
 app.use(express.json())
 app.use(cors())
 
 const {
-    seed
+    seed,
+    createUser
 } = require('./controllers/database')
 
 app.post('/api/seed', seed)
+app.post(`/api/createUser`, createUser)
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, '../client/home.html'))
